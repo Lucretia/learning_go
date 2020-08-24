11ec7e3e529c47df8188c410f8e53bc51064d6df
Luke A. Guest <laguest@archeia.com>
2020-08-24 12:19:32 +0100

    Add Exercise: loops and functions.

---
+++ src/loops.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// func Sqrt(x float64) float64 {
+// 	z := 1.0
+
+// 	for i := 0; i < 10; i++ {
+// 		z -= (z*z - x) / (2 * z)
+// 		fmt.Println(z)
+// 	}
+
+// 	return z
+// }
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	oldZ := 0.0
+
+	for {
+		oldZ = z
+		z -= (z*z - x) / (2 * z)
+
+		if math.Abs(oldZ-z) < 0.000000000001 {
+			return z
+		}
+
+		fmt.Println(z)
+	}
+
+	return z
+}
+
+func main() {
+	fmt.Println(" => ", Sqrt(3))
+}
