6665d38d8ffd38eb80a26cae54ec0f98b554f911
Luke A. Guest <laguest@archeia.com>
2020-08-25 07:58:48 +0100

    Add Exercise: binary trees.

---
+++ a_tour_of_go/src/binary_trees.go
@@ -0,0 +1,27 @@
+package main
+
+import "golang.org/x/tour/tree"
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+
+	ch <- t.Value
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	return false
+}
+
+func main() {
+
+}
